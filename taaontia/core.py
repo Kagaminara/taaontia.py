567570176186be915f028088da134632e1b4974b
Gaëtan Flament <misterarkandias@gmail.com>
2019-02-02 09:25:54 +0100

    Added Session setup

--- taaontia/core.py
+++ taaontia/core.py
@@ -1,17 +1,17 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 from models import user
 
 
-def hello():
-    print("Hullo world!")
-
-
 def init(db_path="taaontia.db", debug=False):
     engine = sqlalchemy.create_engine(
         f"sqlite:///{db_path}" if db_path else "sqlite://", echo=debug
     )
-    return engine
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    return Session
