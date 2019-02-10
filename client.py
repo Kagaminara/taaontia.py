33f90f3b151afa6fe907b7ec955ae8f15341eac3
Gaëtan Flament <misterarkandias@gmail.com>
2019-02-10 22:45:44 +0100

    Made the client quit gracefully on Ctrl-D / Ctrl-C

--- client.py
+++ client.py
@@ -2,14 +2,24 @@ from taaontia.commands.registry import commands
 
 
 def test():
-    while True:
-        rawcommand = input("> Type in a command:\n> ")
-        if rawcommand == "exit":
-            break
-        parsed_message = rawcommand.split(" ", 2)
-        print(
-            commands.get(parsed_message[0])(parsed_message[1] if len(parsed_message) > 1 else None)
-        )
+    try:
+        while True:
+            rawcommand = input("> Type in a command:\n> ")
+            if rawcommand == "exit":
+                break
+            parsed_message = rawcommand.split(" ", 2)
+            print(
+                commands.get(parsed_message[0])(
+                    parsed_message[1] if len(parsed_message) > 1 else None
+                )
+            )
+    except (EOFError, KeyboardInterrupt):
+        quit_gracefully()
+
+
+def quit_gracefully():
+    print("Taking a break from your adventure? Fine...")
+    exit()
 
 
 if __name__ == "__main__":
