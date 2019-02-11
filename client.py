fb10130426693856f42f218df1da801c50ae184c
Gaëtan Flament <misterarkandias@gmail.com>
2019-02-11 18:20:30 +0100

    WIP authent

--- client.py
+++ client.py
@@ -1,34 +1,68 @@
+from marshmallow import Schema, fields
+from uuid import uuid4
+from socket import gethostname
+from os import path
+from datetime import datetime
 from taaontia.commands.registry import commands
 from taaontia.core import Taaontia
 
+auth_path = "./auth.data"
 
-def test():
 
-    taaontia = Taaontia()
-    taaontia.init(db_path=None)
+class Authentication(object):
+    def __init__(self, auth_key):
+        self.auth_key = auth_key
+        self.machine_name = gethostname()
+        self.creation_date = datetime.now()
 
-    try:
-        while True:
-            rawcommand = input("> Type in a command:\n> ")
-            if rawcommand == "exit":
-                break
-            parsed_message = rawcommand.split(" ", 2)
-            print(
-                commands.get(parsed_message[0])(
-                    taaontia, parsed_message[1] if len(parsed_message) > 1 else None
+
+class AuthenticationSchema(Schema):
+    auth_key = fields.UUID()
+    machine_name = fields.Str()
+    creation_date = fields.DateTime()
+
+
+class TaaontiaClient(object):
+    """Client to use with taaontia.py"""
+
+    taaontia: Taaontia
+
+    def create_auth_file(self, auth_key):
+        auth = Authentication(auth_key=auth_key)
+
+    def init(self):
+        self.taaontia = Taaontia()
+        self.taaontia.init(db_path=None)
+        if not path.isfile(auth_path):
+            self.taaontia.connect()
+
+    def test(self):
+        try:
+            while True:
+                rawcommand = input("> Type in a command:\n> ")
+                if rawcommand == "exit":
+                    break
+                parsed_message = rawcommand.split(" ", 2)
+                print(
+                    commands.get(parsed_message[0])(
+                        self.taaontia, parsed_message[1] if len(parsed_message) > 1 else None
+                    )
                 )
-            )
-            print("\n")
-    except (EOFError, KeyboardInterrupt):
-        quit_gracefully(taaontia)
+                print("\n")
+        except (EOFError, KeyboardInterrupt):
+            self.quit_gracefully(self.taaontia)
+
+    def quit_gracefully(self, taaontia):
+        self.taaontia.close()
+        print("Taking a break from your adventure? Fine...")
+        exit()
 
 
-def quit_gracefully(taaontia):
-    taaontia.close()
-    print("Taking a break from your adventure? Fine...")
-    exit()
+def main():
+    taaontia_client = TaaontiaClient()
+    taaontia_client.test()
 
 
 if __name__ == "__main__":
-    test()
+    main()
 
--- taaontia/models/user.py
+++ taaontia/models/user.py
@@ -1,13 +1,19 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy_utils.types.password import PasswordType
 from sqlalchemy.orm import relationship
 from taaontia.db.utils import Base
+
+from sqlalchemy_utils import force_auto_coercion
+
+force_auto_coercion()
 
 
 class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
+    password = Column(PasswordType(schemes=["pbkdf2_sha512", "md5_crypt"]), nullable=False)
 
     statistics = relationship("Statistics", uselist=False, back_populates="user")
 
